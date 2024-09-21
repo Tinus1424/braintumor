deaca87aaeb83e90d4c65b969e69a1fb60c6a6eb
Hamza Nadeem Janjua <hamza@Hamzas-MacBook-Pro.local>
2024-09-21 22:33:59 +0200

    Add transfer learning to separate file, add medium model for mnist on our dataset and lenet5

---
+++ transferlearning.py
@@ -0,0 +1,49 @@
+from tensorflow.keras.applications import ResNet50, DenseNet121
+from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.applications.vgg16 import VGG16
+from tensorflow.keras import Sequential, Input, layers
+from tensorflow.keras import regularizers
+import tensorflow as tf
+def transfer_learning(model_class, resolution,X_train, y_train, X_val, y_val, epochs=10, batch_size=32, optimizer: str = "adam"):
+
+    
+    # initialize VGG16 model and make it non trainable. Don't get the last FC layer by setting include_top to false
+    base_model = model_class(weights="imagenet", include_top=False, input_shape=(resolution,resolution,3))
+    base_model.trainable = False 
+    
+    # initialise model 
+    model = Sequential()
+    # add vgg16,resnet,densenet compliant input
+    model.add(Input(shape = (resolution, resolution, 1)))
+    
+    # change our image to resolutionxresolution
+    model.add(layers.Lambda(lambda x: tf.image.resize(x, (resolution, resolution)))) 
+    
+    # change image to rgb
+    model.add(layers.Lambda(lambda x: tf.image.grayscale_to_rgb(x)))
+    
+    # add base model to be used for transfer learning
+    model.add(base_model)
+    model.add(layers.GlobalAveragePooling2D())
+        
+    model.add(layers.Dense(128, activation='relu', kernel_regularizer=regularizers.l2(0.01)))
+    
+    model.add(layers.Dense(4, activation='softmax'))
+    
+    # compile model 
+    model.compile(
+        optimizer=optimizer,
+        loss='categorical_crossentropy',
+        metrics=['accuracy'],
+    )
+    
+    # add early stopping if accuracy does not change for 3 epochs
+    early_stopping = EarlyStopping(monitor='val_accuracy', mode='max', patience=3,  restore_best_weights=True)
+    
+    # fit the model 
+    model.fit(X_train, y_train, epochs=epochs, validation_data = (X_val, y_val), batch_size=batch_size, callbacks=[early_stopping])
+    return model
+
+
+
+
